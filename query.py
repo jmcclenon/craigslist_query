4c0e351673cfab329bb30aa21427affbdc4cf85f
Matt Johnson <mjohn23@gmail.com>
2015-09-07 10:56:02 -0500

    Find all rows

--- query.py
+++ query.py
@@ -1,3 +1,4 @@
+from bs4 import BeautifulSoup as bs4
 import pandas as pd
 import requests
 
@@ -6,4 +7,13 @@ params = dict(bedrooms=1, is_furnished=1)
 rsp = requests.get(url_base, params=params)
 
 # Requests automatically created the correct url
-print rsp.url
+print "Getting data from %s" % rsp.url
+
+# Use BS4 to parse the response
+html = bs4(rsp.text, 'html.parser')
+
+# find_all will pull entries that fit your search criteria
+# We have to use brackets to define attrs
+# Class is a special word in python so we need to make it a string
+apts = html.find_all('p', attrs={'class': 'row'})
+print len(apts)
